609720df9cd8312d7ae7a4f9ff9fcf6bdf46a0f4
raniery <ranie-ry@outlook.com>
2022-10-16 19:52:25 -0300

    feat: transaction model and editor

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,15 @@
+import decimal
 from django.db import models
 
+from transaction_types.models import TransactionType
+
 # Create your models here.
+class Transaction(models.Model):
+    date        = models.DateField()
+    value       = models.DecimalField(decimal_places=6, max_digits=7)
+    cpf         = models.CharField(max_length=11)
+    card_number = models.CharField(max_length=12)
+    time        = models.TimeField()
+    owner       = models.CharField(max_length=14)
+    store       = models.CharField(max_length=19)
+    tr_type     = models.ForeignKey('transaction_types.TransactionType', on_delete=models.SET_NULL, null=True)
--- trasaction_types/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrasactionTypesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trasaction_types'
