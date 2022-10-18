ff0279639a324df5c356d0470a06b9c3474a270a
raniery <ranie-ry@outlook.com>
2022-10-17 23:23:44 -0300

    feat: added string representation to transaction model and changed value field to better reflect data

--- transactions/models.py
+++ transactions/models.py
@@ -6,10 +6,13 @@ from transaction_types.models import TransactionType
 # Create your models here.
 class Transaction(models.Model):
     date        = models.DateField()
-    value       = models.DecimalField(decimal_places=6, max_digits=7)
+    value       = models.DecimalField(decimal_places=2, max_digits=10)
     cpf         = models.CharField(max_length=11)
     card_number = models.CharField(max_length=12)
     time        = models.TimeField()
     owner       = models.CharField(max_length=14)
     store       = models.CharField(max_length=19)
     tr_type     = models.ForeignKey('transaction_types.TransactionType', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.store} - {self.date}'
